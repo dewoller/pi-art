3fe743b0de82c3be0772ddc4c8f7eb4829bcee15
Dennis Wollersheim <dewoller@gmail.com>
2014-02-21 06:39:34 +0000

    Pins class working, putting on and off events onto queue

--- Pins.py
+++ Pins.py
@@ -1,56 +1,35 @@
 import time
 from Queue import Queue
 from threading import Timer
-onPi=False
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
         
 class Pins:
-    class P():
-        if onPi:
-            OFF=GPIO.HIGH
-            ON=GPIO.LOW
-        else:
-            OFF="ON"
-            ON="OFF"
-    def __init__(self):
-        timerq = Queue()
+    def __init__(self, eventQueue):
 
-        self.controlPins=(5,9,13,10)
-        self.lockPinIndex=3;
-        if (onPi) :
-            try:
-                import RPi.GPIO as GPIO
-            except RuntimeError:
-                print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
-            GPIO.setmode(GPIO.BOARD)
-            map(GPIO.setup, self.controlPins, GPIO.OUT, initial=P.OFF)
+        self.controlPins={ 3: 1, 5: 2, 7: 3, 11: 4}
+        GPIO.setmode(GPIO.BOARD)
+        self.eventQueue = eventQueue
+        for pin in self.controlPins.keys():
+            print pin
+            GPIO.setup( pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect( pin, GPIO.BOTH, callback=self.keyEventHandler, bouncetime=200)
 
-
-    def GPIO_output( self, pin, what):
-        if (onPi):
-            GPIO.output(pin, what)
-        else:
-            print "setting pin ", pin, " to state ", what, "at time ", time.time()
-
-    def enablePin( self, n, duration=8):
-        self.GPIO_output(self.controlPins[n], self.P.ON)
-        Timer(duration, self.disablePin, [n]).start()
-
-
-    def disablePin(self, n):
-        self.GPIO_output(self.controlPins[n], self.P.OFF)
-
-    def disableAllPins(self ):
-        map(self.GPIO_output, self.controlPins, self.P.OFF)
-
-
-    def water(self, n, duration=120):
-        if (n<4):
-            self.enablePin(n-1, duration)
-
-    def unlock(self):
-        self.enablePin(self.lockPinIndex, 8)
+    def keyEventHandler (self, pin):
+        print "handling button event from pin %s, number %s, currently %s\n" % (pin,  self.controlPins[ pin ], GPIO.input(pin))
+        self.eventQueue.put( "%s|%s" % (self.controlPins[ pin ], GPIO.input(pin)))
 
 
 
-
+if __name__ == "__main__":
+    q = Queue()
+    p = Pins(q)
+    while (1):
+        print(q)
+        print("waiting")
+        time.sleep(10)
+    
+        
